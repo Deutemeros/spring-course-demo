42cc49807da7bfd91a26d69b57071930a9ef846f
Morgane Officer <amadis.lemore@gmail.com>
2025-02-21 15:14:43 +0100

    fix figure by field id query

--- src/main/java/com/memoria/fastmath/repository/MathFigureRepository.java
+++ src/main/java/com/memoria/fastmath/repository/MathFigureRepository.java
@@ -1,5 +1,6 @@
 package com.memoria.fastmath.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import com.memoria.fastmath.model.MathFigure;
 @Repository
 public interface MathFigureRepository extends CrudRepository<MathFigure, Integer>{
 
+	@Query("FROM Field WHERE id = ?1")
 	public Iterable<MathFigure> findByFieldID(Integer id);
 	
 }
